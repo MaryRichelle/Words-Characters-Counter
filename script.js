913f0e741d48319461d46f3408738cd4baa2ac02
Mary Richelle <mary.richal44@gmail.com>
2023-10-16 21:44:54 +0300

    fixed word counter issue

--- script.js
+++ script.js
@@ -1,42 +1,81 @@
+
+window.addEventListener("DOMContentLoaded", init)
+
+
+function init(){
+
 const textArea = document.getElementById("user-input");
 const totalCounter = document.getElementById("counter");
-const remaining = document.getElementById("remaining");
+const wordCount = document.getElementById("word-count");
+const remainingWords = document.getElementById("remaining-words");
 const wordCounterBtn = document.getElementById("word-counter");
 const characterCounterBtn = document.getElementById("count-characters");
-const discription = document.getElementById("discription")
-function characterCounter() {
-    discription.innerText = "Counting Characters........."
+const description = document.getElementById("description");
+console.log(wordCount);
 
-  textArea.addEventListener("keyup", (event) => {
-    totalCounter.innerText = event.target.value.length;
-    const remainingCharacters = 500 - event.target.value.length;
-    remaining.innerText = remainingCharacters;
-    if (remainingCharacters <= 5) {
-      textArea.style.borderColor = "red";
-      remaining.style.color = "red";
-    }
-    if ((counter) => 494) {
-      totalCounter.style.color = "red";
-    }
-  });
+function characterCounterDescription(status) {
+  description.innerText = `Counting ${status}.........`;
+  textArea.addEventListener("input", updateCharacterCount);
 }
+
+function updateCharacterCount() {
+  const inputText = textArea.value;
+  const characterCount = inputText.length;
+  totalCounter.innerText = characterCount;
+  const remainingCharacters = 100 - characterCount;
+  remainingWords.innerText = remainingCharacters;
+
+  if (remainingCharacters <= 5 || remainingCharacters >= 50) {
+    textArea.style.borderColor = "red";
+    remainingWords.style.color = "red";
+  } else {
+    textArea.style.borderColor = "";
+    remaining.style.color = "";
+  }
+
+  if (remainingCharacters >= 494) {
+    totalCounter.style.color = "red";
+  } else {
+    totalCounter.style.color = "";
+  }
+}
+
 function countWords() {
-
-    discription.innerText = "Counting Words......"
-  textArea.addEventListener("input", (event) => {
-    const theString = event.target.value;
-    const splitString = theString.split(" ");
-    totalCounter.innerText = splitString.length;
-    const remainingWords = 450 -splitString.length
-    remaining.innerText = remainingWords;
-
-  });
+  description.innerText = "Counting Words......";
+  textArea.addEventListener("input", updateWordCount);
 }
+
+function updateWordCount() {
+  const inputText = textArea.value;
+  const words = inputText.split(/\s+/).filter(word => word !== '');
+  const wordCountValue = words.length;
+  console.log(wordCountValue);
+  totalCounter.innerText = wordCountValue;
+  const remainingWordCount = 1000 - wordCountValue;
+  remainingWords.innerText = remainingWordCount;
+}
+
 wordCounterBtn.addEventListener("click", () => {
-  console.log("wordCounterBtn");
+  
+  characterCounterBtn.disabled = false;
+  characterCounterBtn.style.backgroundColor="#f1eded"
+  characterCounterBtn.style.color="#aba9a9"
+  wordCounterBtn.disabled = true;
+  textArea.value = '';
+  characterCounterDescription("Words")
+    updateWordCount();
   countWords();
 });
+
 characterCounterBtn.addEventListener("click", () => {
-  console.log("characterCounterBtn");
-  characterCounter();
+  characterCounterBtn.disabled = true;
+  wordCounterBtn.disabled = false;
+  wordCounterBtn.style.backgroundColor = "#f1eded"
+  wordCounterBtn.style.color = "#aba9a9"
+  textArea.value = '';
+  updateCharacterCount();
+  updateWordCount();
+  characterCounterDescription("Characters")
 });
+
+}
